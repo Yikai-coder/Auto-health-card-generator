c83849de4a99efc43ad955ae516a54ce6c18a15e
Sinmiuybbggh <54537903+Sinmiuybbggh@users.noreply.github.com>
2020-05-20 15:34:25 +0800

    更新可以自由选择开始日期

--- CopyHealthCard.py
+++ CopyHealthCard.py
@@ -13,22 +13,23 @@ import os
 #str="D:\VSCodeProject\PythonProject\copyhealthycard\190110419李怡凯.docx"
 addressIn=input("请输入文件绝对地址，注意后缀应该为docx")
 addressOut=input("请输入保存地址")
+begin = input("请输入开始日期（YYYY-MM-DD）")
 document=Document(r"%s"%addressIn)
-begin=datetime.strptime('2020-2-24','%Y-%m-%d')
+begin=datetime.strptime('%s'%begin,'%Y-%m-%d')
 end=datetime.now()
 b=begin.date()
 e=end.date()
 for i in range(len(document.paragraphs)):
         if len(document.paragraphs[i].text.replace(' ',''))>4:
             print("第"+str(i)+"段的内容是："+document.paragraphs[i].text)
-par=input("请输入日期所在的段落号，注意请先确认日期处为空“ 月 日"，否则日期无法自动修改")
+par=input("请输入日期所在的段落号，注意请先确认日期处为空（2020年 月 日），否则日期无法自动修改")
 par=int(par)
 for i in range((e - b).days+1): 
     myday = b + timedelta(days=i) 
     os.system("date 2020/%d/%d" %(myday.month, myday.day))
     document=Document(r"%s"%addressIn)
     paragraphs = document.paragraphs
-    text = re.sub(" 月 号", "%d月%d号"%(myday.month,myday.day), paragraphs[par].text)
+    text = re.sub(" 月 ", "%d月%d"%(myday.month,myday.day), paragraphs[par].text)
     paragraphs[par].text = text
     if myday.day <10:
         saveadd=addressOut+"\healthcard0%d0%d.docx"%(myday.month, myday.day)
