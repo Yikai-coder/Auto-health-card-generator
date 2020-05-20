b453db588610c851214a120fb607cac1d1adb522
Sinmiuybbggh <54537903+Sinmiuybbggh@users.noreply.github.com>
2020-05-20 16:11:33 +0800

    更新保存文件名前缀自定义

--- CopyHealthCard.py
+++ CopyHealthCard.py
@@ -9,10 +9,9 @@ from docx.enum.text import WD_PARAGRAPH_ALIGNMENT
 import re
 import os
 
-
-#str="D:\VSCodeProject\PythonProject\copyhealthycard\190110419李怡凯.docx"
 addressIn=input("请输入文件绝对地址，注意后缀应该为docx")
 addressOut=input("请输入保存地址")
+save_name = input("请输入保存的文件名前缀（最终格式为文件名前缀MM-DD)")
 begin = input("请输入开始日期（YYYY-MM-DD）")
 document=Document(r"%s"%addressIn)
 begin=datetime.strptime('%s'%begin,'%Y-%m-%d')
@@ -32,10 +31,10 @@ for i in range((e - b).days+1):
     text = re.sub(" 月 ", "%d月%d"%(myday.month,myday.day), paragraphs[par].text)
     paragraphs[par].text = text
     if myday.day <10:
-        saveadd=addressOut+"\healthcard0%d0%d.docx"%(myday.month, myday.day)
+        saveadd=addressOut + "\\" + save_name +  "0%d0%d.docx"%(myday.month, myday.day)
         document.save("%s"%saveadd)
     else:
-        saveadd = addressOut + "\healthcard0%d%d.docx"%(myday.month, myday.day)
+        saveadd = addressOut + "\\" +save_name + "0%d%d.docx"%(myday.month, myday.day)
         document.save("%s"%saveadd)
 
     
